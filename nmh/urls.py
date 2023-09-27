54ff72f1e0db06102ccf1e8e80a3e9212c904f67
BDlhj <akk808@naver.com>
2023-09-27 16:41:36 +0900

    feat: Add url path of accounts app to urlpatterns of nmh/urls.py

--- nmh/urls.py
+++ nmh/urls.py
@@ -19,4 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("mails/", include("mails.urls")),
+    path("api/accounts/", include("accounts.urls")),
 ]
