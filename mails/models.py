b38cb02044dad193fba342b675db38c01cdf3490
suhyuk <isuh88@snu.ac.kr>
2023-09-27 21:47:43 +0900

    feat: mail model and serializer added

--- mails/models.py
+++ mails/models.py
@@ -1,1 +1,18 @@
 # Create your models here.
+from django.db import models
+
+
+class Email(models.Model):
+    subject = models.CharField(max_length=100)
+    message = models.TextField()
+    writer = models.CharField(max_length=20)
+    recipient_list = models.TextField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    edited_at = models.DateTimeField(auto_now=True)
+    is_sent = models.BooleanField(default=False)
+    sent_at = models.DateTimeField(null=True)
+    is_successfully_sent = models.BooleanField(default=False)
+    is_read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.subject
