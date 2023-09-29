2e53030ce0c4532aa4efe51cd157c55da1231d10
BDlhj <akk808@naver.com>
2023-09-29 23:37:13 +0900

    refactor: Restructure User

--- accounts/models.py
+++ accounts/models.py
@@ -1,21 +1,28 @@
-from django.contrib.auth.models import AbstractUser, PermissionsMixin
-from django.db import models
+from django.contrib.auth import models as auth_models
+from django.contrib.auth import validators
+from django.db import models as db_models
 
-from accounts.managers import CustomUserManager
+from accounts import managers
 
 
-class User(AbstractUser, PermissionsMixin):
-    objects = CustomUserManager()
+class User(auth_models.AbstractBaseUser, auth_models.PermissionsMixin):
+    username_validator = validators.UnicodeUsernameValidator()
 
-    username = models.CharField(max_length=20, unique=True)
-    nickname = models.CharField(max_length=20, unique=True)
-    email = models.EmailField(max_length=100, unique=True)
-    is_reader = models.BooleanField(default=False)
-    is_writer = models.BooleanField(default=False)
-    is_superuser = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    username = db_models.CharField(
+        max_length=150, unique=True, validators=[username_validator]
+    )
+    nickname = db_models.CharField(max_length=150, unique=True)
+    email = db_models.EmailField(unique=True)
+    is_reader = db_models.BooleanField(default=False)
+    is_writer = db_models.BooleanField(default=False)
+    is_staff = db_models.BooleanField(default=False)
+    is_active = db_models.BooleanField(default=True)
+    created_at = db_models.DateTimeField(auto_now_add=True)
+    is_superuser = db_models.BooleanField(default=False)
 
+    objects = managers.UserManager()
+
+    EMAIL_FIELD = "email"
     USERNAME_FIELD = "username"
     REQUIRED_FIELDS = ["nickname", "email"]
 
