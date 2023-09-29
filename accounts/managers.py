30aa57dc743b20f2e3c43ac852e48f873fc669d1
BDlhj <akk808@naver.com>
2023-09-29 23:28:12 +0900

    rename: CustomUserManager to UserManager

--- accounts/managers.py
+++ accounts/managers.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.base_user import BaseUserManager
 
 
-class CustomUserManager(BaseUserManager):
+class UserManager(BaseUserManager):
     def create_user(self, username, password, nickname, email, **extra_fields):
         if not email:
             raise ValueError("The Email must be set!")
