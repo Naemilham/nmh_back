4715e4de59a281f7303514dc06d05b845140b633
BDlhj <akk808@naver.com>
2023-10-01 01:21:27 +0900

    feat: Add SigninView, SignoutView to accounts/views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,14 @@
+from dj_rest_auth import views as dj_auth_views
 from dj_rest_auth.registration import views as dj_reg_views
 
 
 class SignupView(dj_reg_views.RegisterView):
+    pass
+
+
+class SigninView(dj_auth_views.LoginView):
+    pass
+
+
+class SignoutView(dj_auth_views.LogoutView):
     pass
