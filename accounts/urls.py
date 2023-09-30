65f203a59fc11f0b2c5b1e2eed2607bc3a2de7a6
BDlhj <akk808@naver.com>
2023-10-01 01:23:25 +0900

    feat: Add urls regarding signin&out and token to accounts/urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,12 @@
 from django import urls
+from rest_framework_simplejwt import views as jwt_views
 
-from accounts import views
+from accounts import views as accounts_views
 
 urlpatterns = [
-    urls.path("signup/", views.SignupView.as_view()),
+    urls.path("signup/", accounts_views.SignupView.as_view()),
+    urls.path("signin/", accounts_views.SigninView.as_view()),
+    urls.path("signout/", accounts_views.SignoutView.as_view()),
+    urls.path("api/token/", jwt_views.TokenObtainPairView.as_view()),
+    urls.path("api/token/refresh/", jwt_views.TokenRefreshView.as_view()),
 ]
