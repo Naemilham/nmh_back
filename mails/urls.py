aee52f1154d588d81b0e8d8d2fe16217d27e974a
suhyuk <isuh88@snu.ac.kr>
2023-10-04 22:14:05 +0900

    fix: unnecessary url string removed

--- mails/urls.py
+++ mails/urls.py
@@ -5,5 +5,5 @@ from .views import EmailSaveView, EmailView
 app_name = "mails"
 urlpatterns = [
     path("", EmailView.as_view()),
-    path("/content", EmailSaveView.as_view()),
+    path("content", EmailSaveView.as_view()),
 ]
