786bb8904f928b049b869812fd6f728221cd03e9
suhyuk <isuh88@snu.ac.kr>
2023-10-04 22:28:54 +0900

    chore: start UserDetailView

--- accounts/views.py
+++ accounts/views.py
@@ -3,3 +3,15 @@ from dj_rest_auth.registration import views as dj_reg_views
 
 class SignupView(dj_reg_views.RegisterView):
     pass
+
+
+# TODO: define UserDetailView for retrieve, update, delete user info using dj_rest_auth
+class UserDetailView(dj_reg_views.UserDetailsView):
+    def get(self, request):
+        pass
+
+    def put(self, request):
+        pass
+
+    def delete(self, request):
+        pass
