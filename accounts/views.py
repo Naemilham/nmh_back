6026f3ab872645f83210eaf4611d1dc392ea9189
suhyuk <isuh88@snu.ac.kr>
2023-10-06 01:58:19 +0900

    fix: change views using dj_rest_auth

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,10 @@
 from dj_rest_auth.registration import views as dj_reg_views
-from rest_framework.generics import RetrieveUpdateDestroyAPIView
-from rest_framework.response import Response, status
+from dj_rest_auth.views import UserDetailsView, UserListView
+from rest_framework.permissions import IsAuthenticated
 
-from .models import ReaderProfile, WriterProfile
-from .serializers import ReaderProfileSerializer, WriterProfileSerializer
+from accounts.serializers import (
+    ReaderProfileSerializer, UserSerializer, WriterProfileSerializer,
+)
 
 
 class SignupView(dj_reg_views.RegisterView):
@@ -11,29 +12,19 @@ class SignupView(dj_reg_views.RegisterView):
 
 
 # TODO: define UserInfoView for retrieve, update, delete user info using dj_rest_auth
-class UserInfoView(RetrieveUpdateDestroyAPIView):
-    def get(self, request):
-        if not request.user.is_authenticated:
-            return Response(
-                {"detail": "로그인 후 다시 시도해주세요."}, status=status.HTTP_401_UNAUTHORIZED
-            )
-        user = request.user
-        # isWriter, isReader 체크 후 writer, reader profile 반환
+class UserInfoView(UserDetailsView):
+    permission_classes = [IsAuthenticated]
+
+    def get_serializer_class(self):
+        user = self.request.user
+
         if user.is_writer:
-            writer_profile = WriterProfile.objects.get(user=user)
-            serializer = WriterProfileSerializer(writer_profile)
-            return Response(serializer.data, status=status.HTTP_200_OK)
+            return WriterProfileSerializer
+        elif user.is_reader:
+            return ReaderProfileSerializer
         else:
-            reader_profile = ReaderProfile.objects.get(user=user)
-            serializer = ReaderProfileSerializer(reader_profile)
-            return Response(serializer.data, status=status.HTTP_200_OK)
-
-    def put(self, request):
-        pass
-
-    def delete(self, request):
-        pass
+            return UserSerializer
 
 
-class UserListView(dj_reg_views.UserListView):
+class UserListView(UserListView):
     pass
