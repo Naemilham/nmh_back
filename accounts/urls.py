ca5187f5b5fd2170d60caa098bfaee251552a583
suhyuk <isuh88@snu.ac.kr>
2023-10-06 17:51:01 +0900

    fix: urls fixed

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,7 @@ from accounts import views
 
 urlpatterns = [
     urls.path("signup/", views.SignupView.as_view()),
-    urls.path("/writers", views.WriterListView()),
-    urls.path("/readers", views.ReaderListView()),
-    urls.path("/info/", views.UserInfoView.as_view()),
+    urls.path("writers/", views.WriterListView().as_view()),
+    urls.path("readers/", views.ReaderListView().as_view()),
+    urls.path("info/", views.UserInfoView.as_view()),
 ]
