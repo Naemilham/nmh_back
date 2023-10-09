26567df254185fb386a309a28d75f8c7880b4f77
BDlhj <akk808@naver.com>
2023-10-09 17:48:05 +0900

    feat: Create SendVerificationEmailView in accounts/views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,16 +1,19 @@
 from dj_rest_auth import views as dj_auth_views
 from dj_rest_auth.registration import views as dj_reg_views
 from dj_rest_auth.views import UserDetailsView
+from rest_framework import generics, status
 from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from accounts.serializers import (
     ReaderProfileSerializer,
+    SendVerificationEmailSerializer,
     UserSerializer,
     WriterProfileSerializer,
 )
 
-from .models import User
+from .models import User, VerificationEmail
 
 
 class SignupView(dj_reg_views.RegisterView):
@@ -23,7 +26,40 @@ class SigninView(dj_auth_views.LoginView):
 
 class SignoutView(dj_auth_views.LogoutView):
     pass
- 
+
+
+class SendVerificationEmailView(generics.CreateAPIView):
+    queryset = VerificationEmail.objects.all()
+    serializer_class = SendVerificationEmailSerializer
+
+    def create(self, request, *args, **kwargs):
+        """
+        VerificationEmail 테이블에 튜플을 추가하고 인증 메일을 발송하는 메소드
+        """
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+
+        email = self.get_queryset().filter(**serializer.validated_data).first()
+        if email and not email.is_verified:
+            email.send(request)
+
+        return Response(
+            {"detail": "인증 메일이 발송되었습니다."},
+            status=status.HTTP_201_CREATED,
+            headers=headers,
+        )
+
+
+class ResendVerificationEmailView(generics.UpdateAPIView):
+    pass
+
+
+class VerifyEmailView(generics.RetrieveAPIView):
+    pass
+
+
 # TODO: define UserInfoView for retrieve, update, delete user info using dj_rest_auth
 class UserInfoView(UserDetailsView):
     permission_classes = [IsAuthenticated]
@@ -47,5 +83,3 @@ class WriterListView(ListAPIView):
 class ReaderListView(ListAPIView):
     query_set = User.objects.filter(is_reader=True)
     serializer_class = ReaderProfileSerializer
-
-
