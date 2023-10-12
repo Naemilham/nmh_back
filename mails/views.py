c3e29bdc87deebc2f779ebf4b26d2aa89d4ecd0d
suhyuk <isuh88@snu.ac.kr>
2023-10-12 21:45:15 +0900

    feat: creat email retrieve view

--- mails/views.py
+++ mails/views.py
@@ -1,4 +1,4 @@
-from django.core.mail import EmailMessage, send_mail
+from django.core.mail import EmailMessage
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -9,21 +9,37 @@ from .models import Email
 from .serializer import EmailSerializer
 
 
-class EmailSendView(APIView):
-    def get(self, request):  # db에 저장된 이메일을 보내는 것으로 바꿔야 함, 임시로 GET으로 구현
+class EmailView(APIView):
+    def get(self, request):
+        # DB에서 주어진 id에 맞는 이메일 반환
+        email_id = request.data.get("email_id")
+
+        try:
+            email = Email.objects.get(id=email_id)
+            response = Response(
+                data=EmailSerializer(email).data, status=status.HTTP_200_OK
+            )
+        except Email.DoesNotExist:
+            response = Response(status=status.HTTP_404_NOT_FOUND)
+
+        return response
+
+    def post(self, request):
+        # DB에서 가져온 이메일을 보내는 것으로 바꿔야 함
+        email_id = request.data.get("email_id")
+        try:
+            email = Email.objects.get(id=email_id)
+        except Email.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        # subject, message, writer는 DB에서 가져오고 recipient_list는 writer의 writerProfile에서 subscribing_readers를 가져옴
+
         subject = request.data.get("subject")
         message = request.data.get("message")
         recipient_list = request.data.get("recipient_list")
 
         if subject is None or message is None or recipient_list is None:
             return Response(status=status.HTTP_400_BAD_REQUEST)
-
-        email = EmailMessage(
-            subject=subject,
-            body=message,
-            from_email=DEFAULT_FROM_EMAIL,
-            bcc=recipient_list,
-        )
 
         success_count = 0  # Count the number of successful sends
 
@@ -68,49 +84,13 @@ class EmailSendView(APIView):
 
         return response
 
-    def post(self, request):
-        email_id = request.data.get("email_id")
-        try:
-            email = Email.objects.get(id=email_id)
-        except Email.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-
-        sucessful_post = send_mail(
-            email.subject,
-            email.message,
-            from_email=None,
-            recipient_list=email.recipient_list,
-        )
-
-        if sucessful_post == len(email.recipient_list):
-            email.is_sent = True
-            email.is_successfully_sent = True
-            response = Response(
-                status=status.HTTP_200_OK,
-                data={"message": "success for all recipients"},
-            )
-        elif sucessful_post == 0:
-            email.is_sent = False
-            email.is_successfully_sent = False
-            response = Response(
-                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                data={"message": "failed for all recipients"},
-            )
-        else:
-            email.is_sent = True
-            email.is_successfully_sent = False
-            response = Response(
-                status=status.HTTP_207_MULTI_STATUS,
-                data={"message": "fail for some recipients"},
-            )
-        return response
-
 
 class EmailSaveView(APIView):
     def post(self, request):
         subject = request.data.get("subject")
         message = request.data.get("message")
         writer = request.data.get("writer")
+
         serializer = EmailSerializer(subject=subject, message=message, writer=writer)
 
         if serializer.is_valid():
