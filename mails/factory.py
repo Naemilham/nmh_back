5f8128ec3c8709c9a29816e83c05d0d66302883c
suhyuk <isuh88@snu.ac.kr>
2023-11-03 15:34:55 +0900

    test: mails app test

--- mails/factory.py
+++ mails/factory.py
@@ -11,5 +11,6 @@ class EmailFactory(factory.django.DjangoModelFactory):
     message = factory.Faker("paragraph", nb_sentences=4)
     writer = factory.Faker("first_name")
     is_sent = factory.Faker("boolean", chance_of_getting_true=50)
+    sent_at = factory.Faker("date_time")
     is_successfully_sent = factory.Faker("boolean", chance_of_getting_true=50)
     is_read = factory.Faker("boolean", chance_of_getting_true=50)
