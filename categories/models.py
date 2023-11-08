38a112846d0e163f2f319879529b67f46e5bff0e
suhyuk <isuh88@snu.ac.kr>
2023-11-08 21:18:20 +0900

    feat: email and category related with relationship

--- categories/models.py
+++ categories/models.py
@@ -4,3 +4,14 @@ from django.db import models
 class Category(models.Model):
     category_id = models.AutoField(primary_key=True)
     category_name = models.CharField(max_length=100)
+    related_mails = models.ManyToManyField("Email", through="EmailCategory")
+
+
+class EmailCategory(models.Model):
+    id = models.AutoField(primary_key=True)
+    email_id = models.ForeignKey(
+        "Email", on_delete=models.CASCADE, related_name="email_category"
+    )
+    category_id = models.ForeignKey(
+        "Category", on_delete=models.CASCADE, related_name="category_email"
+    )
--- mails/models.py
+++ mails/models.py
@@ -17,6 +17,8 @@ class Email(models.Model):
     edited_at = models.DateTimeField(auto_now=True)
     sent_at = models.DateTimeField(null=True)
 
+    categories = models.ManyToManyField("Category", through="EmailCategory")
+
     def __str__(self):
         return self.subject
 
