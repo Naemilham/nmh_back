1d5c7d6171cede28442b916d8f39e2f410f5c359
BDlhj <akk808@naver.com>
2023-11-08 20:09:35 +0900

    fix: Correct on_delete option

--- subscription/models.py
+++ subscription/models.py
@@ -5,9 +5,9 @@ from accounts import models as accounts_models
 
 class Subscription(db_models.Model):
     subscribed_user = db_models.ForeignKey(
-        accounts_models.WriterProfile, on_delete=True
+        accounts_models.WriterProfile, on_delete=db_models.CASCADE
     )
     subscribing_user = db_models.ForeignKey(
-        accounts_models.ReaderProfile, on_delete=True
+        accounts_models.ReaderProfile, on_delete=db_models.CASCADE
     )
     created_at = db_models.DateTimeField(auto_now_add=True)
