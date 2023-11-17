2807458623087d69328d824142d1cbcfe0886c72
BDlhj <akk808@naver.com>
2023-11-17 22:32:41 +0900

    feat: Implement UnsubscribeView

--- subscription/urls.py
+++ subscription/urls.py
@@ -4,4 +4,5 @@ from subscription import views
 
 urlpatterns = [
     urls.path("subscribe/", views.SubscribeView.as_view()),
+    urls.path("unsubscribe/<int:pk>/", views.UnsubscribeView.as_view()),
 ]
--- subscription/views.py
+++ subscription/views.py
@@ -7,3 +7,9 @@ class SubscribeView(generics.CreateAPIView):
     permission_classes = [permissions.IsReader]
     queryset = models.Subscription.objects.all()
     serializer_class = serializers.SubscribeSerializer
+
+
+class UnsubscribeView(generics.DestroyAPIView):
+    permission_classes = [permissions.IsReader]
+    queryset = models.Subscription.objects.all()
+    serializer_class = serializers.SubscribeSerializer
