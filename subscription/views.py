a2bad97b27e2a53c855b97e13ead6ca80dfbce6a
BDlhj <akk808@naver.com>
2023-11-17 21:57:55 +0900

    feat: Add permission in SubscribeView

--- subscription/views.py
+++ subscription/views.py
@@ -1,8 +1,9 @@
 from rest_framework import generics
 
-from subscription import models, serializers
+from subscription import models, permissions, serializers
 
 
 class SubscribeView(generics.CreateAPIView):
+    permission_classes = [permissions.IsReader]
     queryset = models.Subscription.objects.all()
     serializer_class = serializers.SubscribeSerializer
