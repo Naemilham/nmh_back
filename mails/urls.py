9ddc14466848616433954feaa28dceeb2d9d5e6e
suhyuk <isuh88@snu.ac.kr>
2023-11-23 15:06:57 +0900

    fix: reply view url

--- mails/urls.py
+++ mails/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import EmailDetailView, EmailListView, EmailSendView
+from .views import EmailDetailView, EmailListView, EmailReplyView, EmailSendView
 
 app_name = "mails"
 urlpatterns = [
     path("", EmailListView.as_view()),
     path("<int:pk>/", EmailDetailView.as_view()),
     path("send/", EmailSendView.as_view()),
+    path("reply/", EmailReplyView.as_view()),
 ]
