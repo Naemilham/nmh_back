30e9585a2e3562f1ae9f823dc92e6abc42dc9c08
suhyuk <isuh88@snu.ac.kr>
2023-11-23 11:34:39 +0900

    fix: category view fixed with generic API view

--- categories/views.py
+++ categories/views.py
@@ -1,40 +1,14 @@
-from rest_framework import status
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import generics
 
 from .models import Category
 from .serializers import CategorySerializer
 
 
-class CategoryListView(APIView):
-    def get(self, request):
-        categories = Category.objects.all()
-        return Response(status=status.HTTP_200_OK, data=categories)
+class CategoryListView(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
 
 
-class CategoryDetailView(APIView):
-    def get(self, request):
-        category_name = request.data.get("category_name")
-
-        if category_name is None:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
-
-        try:
-            category = Category.objects.get(category_name=category_name)
-            return Response(status=status.HTTP_200_OK, data=category)
-        except Category.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-
-    def post(self, request):
-        category_name = request.data.get("category_name")
-
-        if category_name is None:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
-
-        serializer = CategorySerializer(data=request.data)
-
-        if serializer.is_valid():
-            serializer.save()
-            return Response(status=status.HTTP_201_CREATED)
-        else:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
+class CategoryDetailView(generics.RetrieveUpdateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
