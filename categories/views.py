9e897cfab894178bf1316858436a00a2d4ae5119
suhyuk <isuh88@snu.ac.kr>
2023-11-23 11:48:16 +0900

    feat: email, writer related to category

--- categories/views.py
+++ categories/views.py
@@ -1,7 +1,11 @@
 from rest_framework import generics
 
-from .models import Category
-from .serializers import CategorySerializer
+from .models import Category, EmailCategory, WriterCategory
+from .serializers import (
+    CategorySerializer,
+    EmailCategorySerializer,
+    WriterCategorySerializer,
+)
 
 
 class CategoryListView(generics.ListCreateAPIView):
@@ -12,3 +16,13 @@ class CategoryListView(generics.ListCreateAPIView):
 class CategoryDetailView(generics.RetrieveUpdateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+
+class EmailCategoryListView(generics.ListCreateAPIView):
+    queryset = EmailCategory.objects.all()
+    serializer_class = EmailCategorySerializer
+
+
+class WriterCategoryListView(generics.ListCreateAPIView):
+    queryset = WriterCategory.objects.all()
+    serializer_class = WriterCategorySerializer
