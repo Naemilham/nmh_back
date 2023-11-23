421f5ddebddf8266a4b89a9cc7abff44a50b7f65
suhyuk <isuh88@snu.ac.kr>
2023-11-23 23:48:16 +0900

    fix: email view refactored to generic view and reply skeleton

--- mails/views.py
+++ mails/views.py
@@ -1,3 +1,9 @@
+import html
+import imaplib
+import logging
+import re
+
+from django.conf import settings
 from django.core.mail import EmailMessage
 from django.template.loader import render_to_string
 from rest_framework import generics, status
@@ -9,15 +15,41 @@ from nmh.settings import DEFAULT_FROM_EMAIL
 from .models import Email
 from .serializer import EmailSerializer
 
+logger = logging.getLogger("mails")
+
 
 class EmailListView(generics.ListCreateAPIView):
     queryset = Email.objects.all()
     serializer_class = EmailSerializer
 
+    def list(self, request, *args, **kwargs):
+        logger.info(f"List operation requested by user: {request.user}")
+        return super().list(request, *args, **kwargs)
+
+    def create(self, request, *args, **kwargs):
+        logger.info(f"Create operation requested by user: {request.user}")
+        return super().create(request, *args, **kwargs)
+
 
 class EmailDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Email.objects.all()
     serializer_class = EmailSerializer
+
+    def retrieve(self, request, *args, **kwargs):
+        logger.info(f"Retrieve operation requested by user: {request.user}")
+        return super().retrieve(request, *args, **kwargs)
+
+    def update(self, request, *args, **kwargs):
+        logger.info(f"Update operation requested by user: {request.user}")
+        return super().update(request, *args, **kwargs)
+
+    def partial_update(self, request, *args, **kwargs):
+        logger.info(f"Partial update operation requested by user: {request.user}")
+        return super().partial_update(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        logger.info(f"Destroy operation requested by user: {request.user}")
+        return super().destroy(request, *args, **kwargs)
 
 
 class EmailSendView(APIView):
@@ -30,21 +62,21 @@ class EmailSendView(APIView):
             return Response(status=status.HTTP_404_NOT_FOUND)
 
         subject = email.subject
-        writer = email.writer
         message = email.message
+
+        # 메일 작성자 WriterProfile 객체
+        writer = email.writer
 
         # 메일 카테고리명 리스트
         categories = email.categories.values_list("category_name", flat=True)
 
-        # writer_id = WriterProfile.objects.get(user).id
+        # is_reply_to_me = email.writer.is_reply_to_me
 
         recipient_list = request.data.get("recipient_list")
 
         # 구독 중인 reader들의 이메일 주소를 리스트로 저장
-        # subscibing_readers = WriterProfile.objects.get(id=writer_id).subscribing_readers
+        # subscribing_readers = email.writer.subscribing_readers
         # recipient_list = subscibing_readers.values_list("email", flat=True)
-
-        # subscriber_list 중 메일 수신을 원치 않는 경우 필터링(categories_id 활용)
 
         if subject is None or message is None or recipient_list is None:
             return Response(status=status.HTTP_400_BAD_REQUEST)
@@ -57,19 +89,22 @@ class EmailSendView(APIView):
             "email_template.html",
             {
                 "subject": subject,
-                "writer": writer,
+                "writer": writer.user.username,
                 "message": message,
                 "categories": categories,
             },
         )
 
+        # 고정된 메일 헤더 있을 시 추후 포함
+        formatted_subject = f"[내밀함/{writer.id}] {subject}"
+
         for recipient in recipient_list:
             # 메일 발송 객체 생성
             letter = EmailMessage(
-                subject=subject,
+                subject=formatted_subject,
                 from_email=DEFAULT_FROM_EMAIL,
                 to=[recipient],
-                reply_to=["isuh88@gmail.com"],
+                reply_to=[DEFAULT_FROM_EMAIL],
             )
 
             # 발송 메일 형식을 지정된 HTML 양식으로 포맷
@@ -89,8 +124,10 @@ class EmailSendView(APIView):
 
                 email.save()
             except Exception as e:
-                print(e)  # Need to log this error
+                logger.error(e)
+                return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
+        # 메일 발송에 성공한 수에 따라 응답 메시지를 다르게 전송
         if success_count == len(recipient_list):
             response = Response(
                 status=status.HTTP_200_OK,
@@ -115,5 +152,89 @@ class EmailSendView(APIView):
                     "success_count": success_count,
                 },
             )
+
+        return response
+
+
+class EmailReplyView(APIView):
+    pattern = re.compile(r'charset="UTF-8".*?<div dir=3D"ltr">(.*?)</div>', re.DOTALL)
+
+    def post(self, request):
+        # 메일 서버 연결 후 로그인
+        try:
+            logger.debug("Connecting to mail server...")
+
+            mail = imaplib.IMAP4_SSL(
+                getattr(settings, "EMAIL_HOST_IMAP"),
+                getattr(settings, "EMAIL_PORT_IMAP"),
+                timeout=10,
+            )
+        except Exception as e:
+            logger.error(e)
+            return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+        try:
+            logger.debug("Connecting to mail account...")
+
+            mail.login(
+                getattr(settings, "EMAIL_HOST_USER"),
+                getattr(settings, "EMAIL_HOST_PASSWORD"),
+            )
+        except Exception as e:
+            logger.error(e)
+            return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+        logger.debug("Successfully connected to mail server.")
+
+        # 메일 UTF8 인코딩 설정 및 메일함 선택
+        # mail.enable("UTF8=ACCEPT")                        # naver 메일은 지원하지 않음
+        mail.select("_reply")
+
+        logger.debug("Successfully selected '_reply' mail box.")
+
+        # writer_encoded = request.data.get("writer").encode("utf-8")
+        request.data.get("writer")
+        # logger.debug(f"writer_encoded: {writer}")
+
+        result, data = mail.search(None, "UNSEEN")
+        if result == "OK":
+            email_ids = data[0].split()
+
+        # 읽지 않은 메일이 없을 경우
+        else:
+            logger.info("Failed to fetch email ids from '_reply' mail box.")
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+        logger.info("Successfully fetched email ids from '_reply' mail box.")
+
+        email_content_list = []
+
+        for email_id in email_ids:
+            result, msg_data = mail.fetch(email_id, "(RFC822)")
+            raw_email = msg_data[0][1]
+            email_content_list.append([email_id, raw_email])
+
+            match = self.pattern.search(raw_email.decode("utf-8"))
+            if match:
+                encoded_text = match.group(1)
+                decoded_text = html.unescape(encoded_text)
+                unicode_text = decoded_text = html.unescape(
+                    encoded_text.replace("=", "%").encode("utf-8").decode("utf-8")
+                )
+                # 메일 객체 생성
+                logger.debug(f"Encoded text: {encoded_text}")
+                logger.debug(f"Decoded_text: {decoded_text}")
+                logger.debug(f"Unicode text: {unicode_text}")
+
+        # 메일함 삭제 및 연결 해제
+        mail.close()
+
+        # 메일 서버 로그아웃
+        mail.logout()
+
+        response = Response(
+            status=status.HTTP_200_OK,
+            data={"message": "success", "mail content": email_content_list},
+        )
 
         return response
