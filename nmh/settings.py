bb7cce0f7c5ceb87ab84ff3fd653120c6e5d49fb
suhyuk <isuh88@snu.ac.kr>
2023-11-25 19:40:34 +0900

    conf: csrf origin settings

--- nmh/settings.py
+++ nmh/settings.py
@@ -71,12 +71,18 @@ MIDDLEWARE = [
     "allauth.account.middleware.AccountMiddleware",
     "corsheaders.middleware.CorsMiddleware",
 ]
+# csrf allow settings
+CSRF_TRUSTED_ORIGINS = [
+    "http://127.0.0.1:3000",
+    "http://localhost:3000",
+    "http://183.101.249.46:3000",  # jaewon
+]
 
 # cors allow settings
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:3000",
-    "http://183.101.249.46:3000",
+    "http://183.101.249.46:3000",  # jaewon
 ]
 
 # cookie allow settings
