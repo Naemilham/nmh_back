1c445f9f1d793e5333132296b6bb61d032902c42
BDlhj <akk808@naver.com>
2023-11-25 20:50:57 +0900

    conf: Change DEBUG=True to False and add ip and domains into ALLOWED_HOST

--- nmh/settings.py
+++ nmh/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-env = environ.Env(DEBUG=(bool, True))
+env = environ.Env(DEBUG=(bool, False))
 
 environ.Env.read_env(env_file=os.path.join(BASE_DIR, ".env"))
 
@@ -33,7 +33,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "3.36.252.42", "naemilham.site"]
 
 
 # Application definition
