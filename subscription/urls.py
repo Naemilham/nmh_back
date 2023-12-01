205cc06abe398230240d1f45dacf9df0fc0f166d
BDlhj <akk808@naver.com>
2023-12-02 00:12:21 +0900

    feat: Implement SubscribingWritersListView and SubscribingReadersListView

--- subscription/urls.py
+++ subscription/urls.py
@@ -5,4 +5,6 @@ from subscription import views
 urlpatterns = [
     urls.path("subscribe/", views.SubscribeView.as_view()),
     urls.path("unsubscribe/", views.UnsubscribeView.as_view()),
+    urls.path("subscribing-writers/", views.SubscribingWritersListView.as_view()),
+    urls.path("subscribing-readers/", views.SubscribingReadersListView.as_view()),
 ]
